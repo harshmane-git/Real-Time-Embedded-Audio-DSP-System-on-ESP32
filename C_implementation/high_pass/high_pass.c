e0fd56f81e9d874431b520f789abd6b959d6fd61
Umesh Mangalekar <umeshmangalekar3421@gmail.com>
2026-03-18 12:36:00 +0530

    Implement high-pass filter initialization and processing

---
+++ C_implementation/high_pass/high_pass.c
@@ -0,0 +1,12 @@
+#include "high_pass.h"
+
+void high_pass_init(high_pass_t *hp) {
+    biquad_init(&hp->bq1, hp_s1);
+    biquad_init(&hp->bq2, hp_s2);
+}
+
+void high_pass_process_block(high_pass_t *hp, const float *input, float *output, int len) {
+    float temp[256];
+    biquad_process_block(&hp->bq1, input, temp, len);
+    biquad_process_block(&hp->bq2, temp, output, len);
+}
