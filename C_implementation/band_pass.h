c08cb5ca3fe5071c06f40dc5ab306ce3fe5149fe
Umesh Mangalekar <umeshmangalekar3421@gmail.com>
2026-03-18 12:21:26 +0530

    Add files via upload

--- C_implementation/band_pass.h
+++ C_implementation/band_pass.h
@@ -3,8 +3,16 @@
 
 #include "biquad.h"
 
+// ───────────────────────────────────────────────
+// Coefficients for Band-Pass 800 Hz (4th order)
+// fs = 16000 Hz | Bark-Aligned | 4th Order Butterworth
+// ───────────────────────────────────────────────
+static const float bp_s1[5] = {0.12491506f, 0.00000000f, -0.12491506f, -1.66451047f, 0.75016988f};
+static const float bp_s2[5] = {0.05582542f, 0.00000000f, -0.05582542f, -1.79592677f, 0.88834915f};
+
 typedef struct {
-    biquad_t bq;   // Single biquad for 2nd-order band-pass
+    biquad_t bq1;
+    biquad_t bq2;
 } band_pass_t;
 
 void band_pass_init(band_pass_t *bp);
