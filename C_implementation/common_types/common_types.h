8f44bbb9ff2cd0c9727306130c83b4ca49a49984
Umesh Mangalekar <umeshmangalekar3421@gmail.com>
2026-04-11 16:56:22 +0530

    Add files via upload

--- C_implementation/common_types/common_types.h
+++ C_implementation/common_types/common_types.h
@@ -1,1 +1,12 @@
+#ifndef COMMON_TYPES_H
+#define COMMON_TYPES_H
 
+#include <stdint.h>
+
+/* Status return type used by all modules */
+typedef enum {
+    STATUS_OK = 0,      // Success
+    STATUS_NOT_OK = 1   // Failure / Error
+} STATUS;
+
+#endif /* COMMON_TYPES_H */
