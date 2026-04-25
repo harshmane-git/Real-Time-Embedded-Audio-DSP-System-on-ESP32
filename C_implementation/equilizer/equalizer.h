fc73e53fcaac69c7479777c13dfd776adbebf850
Umesh Mangalekar <umeshmangalekar3421@gmail.com>
2026-04-25 22:30:21 +0530

    Add files via upload

--- C_implementation/equilizer/equalizer.h
+++ C_implementation/equilizer/equalizer.h
@@ -1,3 +1,5 @@
+/* ========================= equalizer.h ========================= */
+
 #ifndef EQUALIZER_H
 #define EQUALIZER_H
 
@@ -7,27 +9,32 @@
 #include "band_pass.h"
 #include "high_pass.h"
 
-typedef struct {
+typedef struct
+{
     low_pass_config_t  low;
     band_pass_config_t mid;
     high_pass_config_t high;
-    float low_gain_db;
-    float mid_gain_db;
-    float high_gain_db;
 } equalizer_config_t;
 
-typedef struct {
-    low_pass_hdl_t  low;
-    band_pass_hdl_t mid;
-    high_pass_hdl_t high;
-    float low_gain_linear;
-    float mid_gain_linear;
-    float high_gain_linear;
+typedef struct
+{
+    low_pass_hdl_t  low_hdl;
+    band_pass_hdl_t mid_hdl;
+    high_pass_hdl_t high_hdl;
 } equalizer_hdl_t;
 
 STATUS equalizer_open(uint32_t *pui32Size);
-STATUS equalizer_init(equalizer_hdl_t *phdl, const equalizer_config_t *psConfig);
-STATUS equalizer_process(equalizer_hdl_t *phdl, const float *pfInput, float *pfOutput, uint32_t ui32NumSamples);
+
+STATUS equalizer_init(equalizer_hdl_t *phdl,
+                      const equalizer_config_t *psConfig);
+
+STATUS equalizer_process(equalizer_hdl_t *phdl,
+                         const float *pfInput,
+                         float *pfLow,
+                         float *pfMid,
+                         float *pfHigh,
+                         uint32_t ui32NumSamples);
+
 STATUS equalizer_close(equalizer_hdl_t *phdl);
 
 #endif
