140f4304706a0b085b5310a49f1912bf04969917
Umesh Mangalekar <umeshmangalekar3421@gmail.com>
2026-06-28 21:55:13 +0530

    header file of biquad.c

---
+++ C_implementation/biquad/biquad.h
@@ -0,0 +1,18 @@
+#ifndef BIQUAD_H
+#define BIQUAD_H
+
+#include <stdint.h>
+#include "common_types.h"
+
+typedef struct {
+    float w[2];
+    float b[3];
+    float a[2];
+} biquad_t;
+
+STATUS biquad_open(uint32_t *pui32Size);
+STATUS biquad_init(biquad_t *phdl, const float coeffs[5]);
+STATUS biquad_process_block(biquad_t *phdl, const float *pfInput, float *pfOutput, uint32_t ui32NumSamples);
+STATUS biquad_close(biquad_t *phdl);
+
+#endif
